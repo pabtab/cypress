b1d033e4a9ea48230927517bc2be399cc194bc81
Pablo Tabares <ptabares@Pablos-MacBook-Pro-3.local>
2022-08-31 18:20:03 -0400

    kickoff project

---
+++ cypress/e2e/contact.cy.js
@@ -0,0 +1,5 @@
+describe("Integration tests automation store", () => {
+  it("should Should be able to sumbit succesful the contact form", () => {
+    cy.visit("https://automationteststore.com/");
+  });
+});
